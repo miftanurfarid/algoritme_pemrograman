7ee80b2e276ced04c59aef03fff1e1c3a29cb25e
miftanurfarid <miftanurfarid@gmail.com>
2023-09-17 21:28:58 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,2 +1,5 @@
-bil = range(11)
-print(bil)
+pernyataan = "Gatot kaca nyanyi di pinggir jalan"
+iKata = pernyataan.find("kaca")
+pernyataanBaru = pernyataan.replace("jalan", "kali")
+print(iKata)
+print(pernyataanBaru)
